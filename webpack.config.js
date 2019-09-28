e415c28a63bbf9c575ce2d5e5b6c2488882e922a
Felipe Osorio <andres@fruver.com>
2019-09-28 01:06:40 -0500

    working in electron

--- webpack.config.js
+++ webpack.config.js
@@ -9,52 +9,46 @@ const envKeys = Object.keys(dotEnv).reduce((prev, next) => {
   return prev;
 }, {});
 
-module.exports = {
-
-  mode: 'development',
-
-  entry: './src/index.js',
-
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'assets/bundle.js'
-  },
-
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  
-  devtool: 'source-map',
-
-  devServer: {
-    //host: '0.0.0.0',
-    port: 9000,
-    hot: true,
-    inline: false,
-    compress: true,
-    watchContentBase: true,
-    historyApiFallback: true
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js(x?)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'assets/bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        'react-dom': '@hot-loader/react-dom'
       }
+    },
+    devtool: 'source-map',
+    devServer: {
+      //host: '0.0.0.0',
+      port: 9000,
+      hot: true,
+      inline: false,
+      compress: true,
+      watchContentBase: true,
+      historyApiFallback: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js(x?)$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys),
+      new CleanWebpackPlugin(),
+      new HTMLWebpackPlugin({
+        title: 'Fruver System Manager',
+        template: './src/index.html'
+      })
     ]
-  },
-
-  plugins: [
-    new webpack.DefinePlugin(envKeys),
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      title: 'Fruver System Manager',
-      template: './src/index.html'
-    })
-  ]
-};
+  }
+];
