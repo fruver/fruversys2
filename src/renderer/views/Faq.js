23ad0cef40a68df85bfb963121a02ee6cb806cc7
Felipe Osorio <andres@fruver.com>
2019-09-29 21:59:09 -0500

    working in electron app

---
+++ src/renderer/views/Faq.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Faq = () => {
+  const title = 'Faq';
+
+  return (
+    <Layout title={title}>
+      {title}
+    </Layout>
+  );
+};
+
+export default Faq;
---
+++ src/renderer/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Home = () => {
+  const title = 'Panel';
+
+  return (
+    <Layout title={title}>
+      {title}
+    </Layout>
+  );
+};
+
+export default Home;
